842e0c2b0e5a29220e2d3b80f33eea36af5340a3
djordjevicm93 <djordjevic083@gmail.com>
2025-04-15 15:02:29 +0200

    done

--- dice.py
+++ dice.py
@@ -1,25 +1,40 @@
-# roll 2D10+20
-# xDy+z
-# y – type of dice to use (e.g. D6, D10)
-# x – number of dice rolled; for a single roll this parameter is omitted
-# z – modifier - number to add (or subtract) to the result of the roll (optional)
-# D3, D4, D6, D8, D10, D12, D20, D100
+import random
 
-from random import randint
+def roll_dice(code):
+    valid_dice = [3, 4, 6, 8, 10, 12, 20, 100]
+    code = code.upper()
+    if "D" not in code:
+        return "Invalid format! Missing 'D'."
+    parts = code.split("D")
+    if parts[0] == '':
+        num_rolls = 1
+    else:
+        try:
+            num_rolls = int(parts[0])
+        except ValueError:
+            return "Invalid number of dice."
+    modifier = 0
+    dice_and_mod = parts[1]
+    if '+' in dice_and_mod:
+        dice_part, mod_part = dice_and_mod.split('+')
+        modifier = int(mod_part)
+    elif '-' in dice_and_mod:
+        dice_part, mod_part = dice_and_mod.split('-')
+        modifier = -int(mod_part)
+    else:
+        dice_part = dice_and_mod
+    try:
+        dice_type = int(dice_part)
+    except ValueError:
+        return "Invalid dice type."
+    if dice_type not in valid_dice:
+        return f"Invalid dice type D{dice_type}."
+    rolls = [random.randint(1, dice_type) for _ in range(num_rolls)]
+    total = sum(rolls) + modifier
+    return f"Rolls: {rolls}, Modifier: {modifier}, Total: {total}"
 
-dice_types = [3, 4, 6, 8, 10, 12, 20, 100]
-
-def dice(score_of_dice):
-    for y in dice_types:
-        if y in score_of_dice:
-            return randint(int(y))
-        else:
-            return "Not in dice types. Select right one."
-        if x in score_of_dice:
-            return sum(x, y)
-        else:
-            return None
-        if z in score_of_dice:
-            return score_of_dice + z or score_of_dice - z
-        return x + y + z or x + y - z
-
+print(roll_dice("2D10+10"))
+print(roll_dice("D6"))
+print(roll_dice("D12-1"))
+print(roll_dice("3D8"))
+print(roll_dice("2D7"))
